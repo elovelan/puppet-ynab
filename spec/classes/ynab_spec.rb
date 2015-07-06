4d17cc02cb0111c8e6458d9deae1503bf323d267
Eric Loveland <eric@ericloveland.com>
2015-07-05 23:21:17 -0400

    update to version 4.3.655

--- spec/classes/ynab_spec.rb
+++ spec/classes/ynab_spec.rb
@@ -4,7 +4,7 @@ describe 'ynab' do
   it do
     should contain_package('YNAB 4').with({
       :provider => 'appdmg',
-      :source   => 'http://downloadpull-youneedabudgetco.netdna-ssl.com/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.195.dmg',
+      :source   => 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.655.dmg',
     })
   end
 end
