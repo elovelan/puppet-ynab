f51a9a64594e407fd042d88e082b4ba579360601
Eric Loveland <eric@ericloveland.com>
2015-07-05 23:21:17 -0400

    update to version 4.3.655

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #  include ynab
 class ynab {
   package { 'YNAB 4':
-    source   => 'http://downloadpull-youneedabudgetco.netdna-ssl.com/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.195.dmg',
+    source   => 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.655.dmg',
     provider => 'appdmg'
   }
 }
