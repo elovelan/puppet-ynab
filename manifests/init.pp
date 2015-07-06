f2dcd8347706be826ead6cba688215535f0bacd4
Eric Loveland <eric@ericloveland.com>
2015-07-05 23:09:23 -0400

    update package name to match app

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #  include ynab
 class ynab {
-  package { 'YNAB':
+  package { 'YNAB 4':
     source   => 'http://downloadpull-youneedabudgetco.netdna-ssl.com/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.195.dmg',
     provider => 'appdmg'
   }
