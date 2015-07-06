37924ba3045b6d0e2d6e0fee84b7b0cd37c23232
Eric Loveland <eric@ericloveland.com>
2015-07-05 22:46:09 -0400

    fix tests

--- spec/classes/adium_spec.rb
+++ spec/classes/adium_spec.rb
@@ -4,7 +4,7 @@ describe 'ynab' do
   it do
     should contain_package('YNAB').with({
       :provider => 'appdmg',
-      :source   => 'http://downloadpull-youneedabudgetco.netdna-ssl.com/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.1.515.dmg',
+      :source   => 'http://downloadpull-youneedabudgetco.netdna-ssl.com/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.195.dmg',
     })
   end
 end
